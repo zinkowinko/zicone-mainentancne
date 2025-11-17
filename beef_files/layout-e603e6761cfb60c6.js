56529e8c0a7502842a759c2298b52a60648abab8
zinko <projectrez69420@gmail.com>
2025-11-16 16:38:35 -0800

    Delete beef_files directory

--- beef_files/layout-e603e6761cfb60c6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{347:()=>{},8352:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.t.bind(n,347,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[690,970,441,587,358],()=>s(8352)),_N_E=e.O()}]);
